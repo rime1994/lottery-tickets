6a52a4034f3b5ddcddcd9c8bfd772e1f16beba6f
我脑袋重了 <1994rime@gmail.com>
2019-03-23 16:50:53 +0800

    update build.c
    加入了单组生产数的重复性效验

--- build.c
+++ build.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int z;//声明全局变量z，用于输入的买入数量计数
-int choice;
+int z;      //声明全局变量z，用于输入的买入数量计数
+int choice; //声明全局变量choice，用于menu的参数，及子函数内返回
 int main()
 {
-    void menu(n);
-    void random_doubleball();
+    void menu(n); //声明函数menu，主要功能由menu实现，通选选择不通的选项调用其他子函数
     printf("choice lottery tickets: \n"
            "1.powerball\n"
            "2.double coloer ball\n"
-           "3.exit\n");
+           "3.exit.\n");
     scanf("%d", &choice);
     menu(choice);
     system("pause");
@@ -18,8 +17,9 @@ int main()
 
 void menu(n)
 {
-    void number();
-    void random_powerball();
+    void number();            //声明number函数，主要用于输入购买数量的效验，并为z赋值
+    void random_powerball();  //声明，powerball算法实现函数
+    void random_doubleball(); //声明，双色球算法实现函数
     switch (n)
     {
     case 1:
@@ -27,6 +27,7 @@ void menu(n)
         random_powerball();
         break;
     case 2:
+        number();
         break;
     case 3:
         exit(0);
@@ -40,9 +41,10 @@ void number()
 {
     printf("How many do you want to buy?\n");
     scanf("%d", &z);
-    if (z > 5 || z <= 0)
+    printf("\n");
+    if (z > 5 || z <= 0) //限制了一次买入的数量，并且如果输入为负数将会报错，返回菜单
     {
-        printf("don't buy too much!\n");
+        printf("don't buy too much! or input error.\n");
         menu(choice);
     }
     else
@@ -52,17 +54,27 @@ void number()
 void random_powerball()
 {
     srand(time(0));
-    int i, j, k;
-    int a[5];
+    int i, j;
+    int a[6];
     for (j = 1; j <= z; j++)
     {
-        printf("%d:\n", j);
+        printf("%02d:\n", j);
         for (i = 0; i <= 4; i++)
         {
-            printf("%02d  ", rand() % 69 + 1);
+            a[i] = rand() % 69 + 1;
+            if (i > 0) //增加了随机生成数的效验功能，后输出的数不会与之前的数相同
+            {
+                if (a[i] == a[i - 1] || a[i] == a[i - 2] || a[i] == a[i - 3] || a[i] == a[i - 4])
+                    i--;
+            }
         }
-        printf("\t");
-        printf("%02d\n\n", rand() % 27 + 1);
+        a[5] = rand() % 27 + 1;
+        for (i = 0; i <= 4; i++)
+        {
+            printf("%02d ", a[i]);
+        }
+        printf("   %02d", a[5]);
+        printf("\n\n");
     }
     return 0;
 }
