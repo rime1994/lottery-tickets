f286cb8bd2ffaebc61cd77f19d9ab2f7ea99bba7
我脑袋重了 <1994rime@gmail.com>
2019-03-23 15:10:20 +0800

    Update build.c
    对代码进行了函数式改造，增加了选择菜单，2选项还不可用。

--- build.c
+++ build.c
@@ -1,32 +1,68 @@
-//这是一段价值5.5亿美金的代码
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int z;//声明全局变量z，用于输入的买入数量计数
+int choice;
 int main()
 {
-    int i, j, k;
-    do
-    {
-        printf("How many lottery tickets do you want to buy?\t");
-        scanf("%d", &k);
-        printf("\n");
-        if (k > 5)
-        {
-            printf("You don't have that much money, buy up to 5 !!!\n\n");
-            continue;
-        }
-        else
-            srand(time(0));
-        for (j = 1; j <= k; j++)
-        {
-            printf("%d:\n", j);
-            for (i = 0; i <= 4; i++)
-            {
-                printf("%2d  ", rand() % 69 + 1);
-            }
-            printf("\t");
-            printf("%2d\n\n", rand() % 27 + 1);
-        }
-    } while (k > 5);
+    void menu(n);
+    void random_doubleball();
+    printf("choice lottery tickets: \n"
+           "1.powerball\n"
+           "2.double coloer ball\n"
+           "3.exit\n");
+    scanf("%d", &choice);
+    menu(choice);
     system("pause");
+}
+
+void menu(n)
+{
+    void number();
+    void random_powerball();
+    switch (n)
+    {
+    case 1:
+        number();
+        random_powerball();
+        break;
+    case 2:
+        break;
+    case 3:
+        exit(0);
+    default:
+        printf("input error! please try again.\n\n");
+        main();
+    }
+    return 0;
+}
+void number()
+{
+    printf("How many do you want to buy?\n");
+    scanf("%d", &z);
+    if (z > 5 || z <= 0)
+    {
+        printf("don't buy too much!\n");
+        menu(choice);
+    }
+    else
+        return (z);
+}
+
+void random_powerball()
+{
+    srand(time(0));
+    int i, j, k;
+    int a[5];
+    for (j = 1; j <= z; j++)
+    {
+        printf("%d:\n", j);
+        for (i = 0; i <= 4; i++)
+        {
+            printf("%02d  ", rand() % 69 + 1);
+        }
+        printf("\t");
+        printf("%02d\n\n", rand() % 27 + 1);
+    }
+    return 0;
 }
