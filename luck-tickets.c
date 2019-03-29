8134feb6c163ae8053a579efc1f3303e88520f6c
我脑袋重了 <fuqi.s@outlook.com>
2019-03-29 18:50:53 +0800

    V1.0
    升级到1.0版本幸运彩票

--- luck-tickets.c
+++ luck-tickets.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int num_buy;    //声明全局变量num_buy，用于输入的买入数量计数
+int num_buy;  //声明全局变量num_buy，用于输入的买入数量计数
 int choice;     //声明全局变量choice，用于menu的参数，及子函数内返回
 int num_choice; //声明全局变量num_choice，用赋值单组彩票生成数个数
-int main(void)
-int main()
+void main(void)
 {
-    void menu(n); //声明函数menu，主要功能由menu实现，通选选择不通的选项调用其他子函数
+    void Menu(n); //声明函数menu，主要功能由menu实现，通选选择不通的选项调用其他子函数
     printf("choice lottery tickets: \n"
            "1.powerball\n"
            "2.double coloer ball\n"
            "3.exit.\n");
     scanf("%d", &choice);
-    menu(choice);
+    Menu(choice);
     system("pause");
 }
-
-void menu(n)
+void Menu(n)
 {
-    void number(void); //声明number函数，主要用于输入购买数量的效验，并为num_buy赋值
-    void random(int first, int second);
-    void print_arr(void); //声明，powerball算法实现函数
-    void number(); //声明number函数，主要用于输入购买数量的效验，并为num_buy赋值
-    void random(int first, int second);
-    void print_arr(); //声明，powerball算法实现函数
-    switch (n)
+    void Number(); //声明number函数，主要用于输入购买数量的效验，并为num_buy赋值
+    void Random(int first, int second);
+    void Print_arr();
+    switch (choice)
     {
     case 1:
         num_choice = 4;
-        number();
-        random(69, 27);
-        break;
+        Number();
+        printf("************************\n");
+        Random(69, 27);
+        printf("************************\n");
+        main();
     case 2:
         num_choice = 5;
-        number();
-        random(33, 16);
-        break;
+        Number();
+        printf("************************\n");
+        Random(33, 16);
+        printf("************************\n");
+        main();
     case 3:
         exit(0);
     default:
@@ -44,19 +43,18 @@ void menu(n)
         main();
     }
 }
-void number()
+void Number()
 {
     printf("How many do you want to buy?\n");
     scanf("%d", &num_buy);
     printf("\n");
-    if (num_buy > 5 || num_buy <= 0) //限制了一次买入的数量，并且如果输入为负数将会报错，返回菜单
+    if (num_buy >= 5 || num_buy <= 0) //限制了一次买入的数量，并且如果输入为负数将会报错，返回菜单
     {
         printf("don't buy too much! or input error.\n");
-        menu(choice);
+        Menu(choice);
     }
 }
-
-void random(first, second)
+void Random(first, second)
 {
     srand(time(0));
     int i, j, k;
@@ -80,6 +78,6 @@ void random(first, second)
             printf("%02d ", a[i]);
         }
         printf("   %02d", a[num_choice + 1]);
-        printf("\n\n");
+        printf("\n------------------------\n\n");
     }
 }
